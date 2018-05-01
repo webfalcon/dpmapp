8547ecec34034e221d79245ae1e660dfdc5b343d
Aram <webfalcon.us@gmail.com>
2018-05-01 10:29:07 +0400

    fixed getVerseById

--- js/components/bible/BibleFunctions.js
+++ js/components/bible/BibleFunctions.js
@@ -9,38 +9,17 @@ export function getChapterTitle(State) {
   return `${State.currentBook.Name} ${State.currentChapter.Number}`;
 }
 
-export function getAllSelectedVersesNumber(State) {
-  const selectedVerses = State.selectedVerses;
-  return `${this.getVerseById(selectedVerses[0]).verse.Number}-
-      ${this.getVerseById(selectedVerses[selectedVerses.length - 1]).verse.Number}`;
-}
-export function getAllSelectedVerses(State) {
-  let verses = '';
-  const selectedVerses = State.selectedVerses;
-  verses += selectedVerses.map(verse => `
-    ${this.getVerseById(verse).verse.Number} 
-    ${this.getVerseById(verse).verse.Content}`);
-
-  return verses;
-}
-
 export function getVerseName(verse, State) {
   return `${getChapterTitle(State)}-${State.currentChapter.Verses[verse].Number}`;
 }
 
 export function getVerseById(id, State) {
   const idArr = id.split(/(\d+)/);
-  const verse = [];
 
-  // verse = idArr.filter(index => idArr.indexOf(index) % 2 === 1);
+  const verse = idArr.filter(index => idArr.indexOf(index) % 2 === 1)
+    .map(index => parseInt(index - 1));
 
-  idArr.map((index) => {
-    if (idArr.indexOf(index) % 2 === 1) {
-      verse.push(parseInt(index - 1));
-    }
-    return false;
-  });
-
+  console.log(verse);
   const currentChapter = State.currentChapter;
   return {
     verse: currentChapter.Verses[verse[4]],
@@ -49,3 +28,17 @@ export function getVerseById(id, State) {
   };
 }
 
+export function getAllSelectedVersesNumber(State) {
+  const selectedVerses = State.selectedVerses;
+  return `${getVerseById(selectedVerses[0]).verse.Number}-
+      ${getVerseById(selectedVerses[selectedVerses.length - 1]).verse.Number}`;
+}
+export function getAllSelectedVerses(State) {
+  let verses = '';
+  const selectedVerses = State.selectedVerses;
+  verses += selectedVerses.map(verse => `
+    ${getVerseById(verse).verse.Number} 
+    ${getVerseById(verse).verse.Content}`);
+
+  return verses;
+}
